6832b139eaccd741027c85045ce9d21d369b2c03
Mohammed Saif <saifmohammed888@gmail.com>
2021-02-27 02:58:16 +0530

    Basics

---
+++ script.js
@@ -0,0 +1,27 @@
+console.log("Basics of JavaScript");
+
+//Variables
+let email = "saifmohammed888@gmail.com"
+const dob = "10/08/1996";
+{
+    var user = "Mohammed Saif";
+}
+console.log(user, email, dob);
+
+
+//Assignment Operator 
+var salary = 40000;
+console.log(salary);
+
+//Arithmetic Operator
+var num = 10;
+console.log('Sum = ' + (10 + 20));
+console.log('Difference = ' + (100 + 20));
+console.log('Product = ' + (5 * 20));
+console.log('Quotient = ' + (15 / 2));
+console.log('Exponention = ' + (2 ** 3));
+console.log('Modulus = ' + (10 % 3));
+console.log('Increment = ' + (++num));
+console.log('Decrement = ' + (--num));
+
+//
